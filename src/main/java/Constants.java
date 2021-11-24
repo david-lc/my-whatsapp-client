2e1f594982df93e68223a0a6c730ced74a9e47cd
David Soler García <60581461+SDABIS@users.noreply.github.com>
2021-11-24 19:17:16 +0100

    Cosas

---
+++ src/main/java/Constants.java
@@ -0,0 +1,6 @@
+import java.math.BigInteger;
+
+public class Constants {
+    public static final BigInteger DIFFIE_HELLMAN_G = BigInteger.valueOf(42);
+    public static final BigInteger DIFFIE_HELLMAN_P = BigInteger.valueOf(42);
+}
---
+++ src/main/java/DiffieHellmanKey.java
@@ -0,0 +1,45 @@
+import javax.crypto.spec.DHParameterSpec;
+import javax.crypto.spec.DHPrivateKeySpec;
+import javax.crypto.spec.DHPublicKeySpec;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+public class DiffieHellmanKey {
+    private BigInteger pubKey;
+    private BigInteger privKey;
+
+    public DiffieHellmanKey() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("DiffieHellman");
+            DHParameterSpec param = new DHParameterSpec(Constants.DIFFIE_HELLMAN_P, Constants.DIFFIE_HELLMAN_G);
+            kpg.initialize(param);
+            KeyPair kp = kpg.generateKeyPair();
+
+            KeyFactory kfactory = KeyFactory.getInstance("DiffieHellman");
+
+            DHPublicKeySpec publicKeySpec = kfactory.getKeySpec(kp.getPublic(), DHPublicKeySpec.class);
+            DHPrivateKeySpec privateKeySpec = kfactory.getKeySpec(kp.getPrivate(), DHPrivateKeySpec.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BigInteger getPubKey() {
+        return pubKey;
+    }
+
+    public void setPubKey(BigInteger pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public BigInteger getPrivKey() {
+        return privKey;
+    }
+
+    public void setPrivKey(BigInteger privKey) {
+        this.privKey = privKey;
+    }
+
+}
